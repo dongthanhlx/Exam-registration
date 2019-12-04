9022563e7839ff6e003aa59b95ea995dff9d2fd8
Đồng Thành <dongthanhlx@gmail.com>
2019-12-04 09:38:50 +0700

    edit forms.tables

--- resources/views/components/tables/studentAccountTable.blade.php
+++ resources/views/components/tables/studentAccountTable.blade.php
@@ -1,48 +1,45 @@
-@extends('layouts.admin')
 
-@section('content')
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @elseif(session()->has('message'))
-        <div class="alert alert-success">
-            {{ session()->get('message') }}
-        </div>
-    @endif
-    <table class="table table-hover">
-        <thead>
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@elseif(session()->has('message'))
+    <div class="alert alert-success">
+        {{ session()->get('message') }}
+    </div>
+@endif
+<table class="table table-hover">
+    <thead>
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">Họ và tên</th>
+        <th scope="col">Mã sinh viên</th>
+        <th scope="col">Email</th>
+        <th scope="col">Mật khẩu</th>
+        <th scope="col"></th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach ($records as $record)
         <tr>
-            <th scope="col">#</th>
-            <th scope="col">Họ và tên</th>
-            <th scope="col">Mã sinh viên</th>
-            <th scope="col">Email</th>
-            <th scope="col">Mật khẩu</th>
-            <th scope="col"></th>
+            <th scope="row">{{ $loop->index + 1 }}</th>
+            <th scope="row">{{ $record->full_name }}</th>
+            <th scope="row">{{ $record->student_code }}</th>
+            <th scope="row">{{ $record->email }}</th>
+            <th scope="row">{{ $record->password }}</th>
+            <th scope="row">
+                <form action="" method="post" class="float-right">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-outline-primary" onclick="return confirm('Chắc không ?')">Delete</button>
+                </form>
+                <a href="#"><button class="btn btn-outline-primary float-right">Edit</button></a>
+            </th>
         </tr>
-        </thead>
-        <tbody>
-        @foreach ($records as $record)
-            <tr>
-                <th scope="row">{{ $loop->index + 1 }}</th>
-                <th scope="row">{{ $record->full_name }}</th>
-                <th scope="row">{{ $record->student_code }}</th>
-                <th scope="row">{{ $record->email }}</th>
-                <th scope="row">{{ $record->password }}</th>
-                <th scope="row">
-                    <form action="" method="post" class="float-right">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-outline-primary" onclick="return confirm('Chắc không ?')">Delete</button>
-                    </form>
-                    <a href="#"><button class="btn btn-outline-primary float-right">Edit</button></a>
-                </th>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-@endsection
+    @endforeach
+    </tbody>
+</table>
--- resources/views/components/tables/studentOfSubjectTable.blade.php
+++ resources/views/components/tables/studentOfSubjectTable.blade.php
@@ -1,50 +1,47 @@
-@extends('layouts.admin')
 
-@section('content')
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @elseif(session()->has('message'))
-        <div class="alert alert-success">
-            {{ session()->get('message') }}
-        </div>
-    @endif
-    <table class="table table-hover">
-        <thead>
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@elseif(session()->has('message'))
+    <div class="alert alert-success">
+        {{ session()->get('message') }}
+    </div>
+@endif
+<table class="table table-hover">
+    <thead>
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">Họ và tên đệm</th>
+        <th scope="col">Tên</th>
+        <th scope="col">Ngày sinh</th>
+        <th scope="col">Mã sinh viên</th>
+        <th scope="col">Mã môn học</th>
+        <th scope="col"></th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach ($records as $record)
         <tr>
-            <th scope="col">#</th>
-            <th scope="col">Họ và tên đệm</th>
-            <th scope="col">Tên</th>
-            <th scope="col">Ngày sinh</th>
-            <th scope="col">Mã sinh viên</th>
-            <th scope="col">Mã môn học</th>
-            <th scope="col"></th>
+            <th scope="row">{{ $loop->index + 1 }}</th>
+            <th scope="row">{{ $record->first_name }}</th>
+            <th scope="row">{{ $record->last_name }}</th>
+            <th scope="row">{{ $record->birthday }}</th>
+            <th scope="row">{{ $record->student_code }}</th>
+            <th scope="row">{{ $record->subject_code }}</th>
+            <th scope="row">
+                <form action="" method="post" class="float-right">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-outline-primary" onclick="return confirm('Chắc không ?')">Delete</button>
+                </form>
+                <a href="#"><button class="btn btn-outline-primary float-right">Edit</button></a>
+            </th>
         </tr>
-        </thead>
-        <tbody>
-        @foreach ($records as $record)
-            <tr>
-                <th scope="row">{{ $loop->index + 1 }}</th>
-                <th scope="row">{{ $record->first_name }}</th>
-                <th scope="row">{{ $record->last_name }}</th>
-                <th scope="row">{{ $record->birthday }}</th>
-                <th scope="row">{{ $record->student_code }}</th>
-                <th scope="row">{{ $record->subject_code }}</th>
-                <th scope="row">
-                    <form action="" method="post" class="float-right">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-outline-primary" onclick="return confirm('Chắc không ?')">Delete</button>
-                    </form>
-                    <a href="#"><button class="btn btn-outline-primary float-right">Edit</button></a>
-                </th>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-@endsection
+    @endforeach
+    </tbody>
+</table>
