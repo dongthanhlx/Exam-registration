6163e8e8a7540aff9759df5589810b8846528f90
Hung Cao <huuhungf@gmail.com>
2019-12-05 23:21:39 +0700

    popup import dssv

--- resources/views/components/tables/studentInfoTable.blade.php
+++ resources/views/components/tables/studentInfoTable.blade.php
@@ -11,38 +11,40 @@
         {{ session()->get('message') }}
     </div>
 @endif
-<table class="table table-hover">
-    <thead>
-    <tr>
-        <th scope="col">#</th>
-        <th scope="col">Họ và tên đệm</th>
-        <th scope="col">Tên</th>
-        <th scope="col">Ngày sinh</th>
-        <th scope="col">Giới tính</th>
-        <th scope="col">Mã sinh viên</th>
-        <th scope="col">Lớp</th>
-        <th scope="col"></th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach ($records as $record)
+<div class="container-fluid pt-5">
+    <table class="table table-hover">
+        <thead>
         <tr>
-            <th scope="row">{{ $loop->index + 1 }}</th>
-            <th scope="row">{{ $record->first_name }}</th>
-            <th scope="row">{{ $record->last_name }}</th>
-            <th scope="row">{{ $record->birthday }}</th>
-            <th scope="row">{{ $record->gender }}</th>
-            <th scope="row">{{ $record->student_code }}</th>
-            <th scope="row">{{ $record->class }}</th>
-            <th scope="row">
-                <form action="" method="post" class="float-right">
-                    @csrf
-                    @method('DELETE')
-                    <button class="btn btn-outline-primary" onclick="return confirm('Chắc không ?')">Delete</button>
-                </form>
-                <a href="#"><button class="btn btn-outline-primary float-right">Edit</button></a>
-            </th>
+            <th scope="col">#</th>
+            <th scope="col">Họ và tên đệm</th>
+            <th scope="col">Tên</th>
+            <th scope="col">Ngày sinh</th>
+            <th scope="col">Giới tính</th>
+            <th scope="col">Mã sinh viên</th>
+            <th scope="col">Lớp</th>
+            <th scope="col"></th>
         </tr>
-    @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+        @foreach ($records as $record)
+            <tr>
+                <th scope="row">{{ $loop->index + 1 }}</th>
+                <th scope="row">{{ $record->first_name }}</th>
+                <th scope="row">{{ $record->last_name }}</th>
+                <th scope="row">{{ $record->birthday }}</th>
+                <th scope="row">{{ $record->gender }}</th>
+                <th scope="row">{{ $record->student_code }}</th>
+                <th scope="row">{{ $record->class }}</th>
+                <th scope="row">
+                    <form action="" method="post" class="float-right">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-outline-primary" onclick="return confirm('Chắc không ?')">Delete</button>
+                    </form>
+                    <a href="#"><button class="btn btn-outline-primary float-right">Edit</button></a>
+                </th>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
