bc2cbfb73533bd0b0f524974669cc6c32d9f8c1e
Hung Cao <huuhungf@gmail.com>
2019-12-11 00:47:26 +0700

    using axios and vuejs to make roomTable 2

--- resources/views/components/tables/roomTable.blade.php
+++ resources/views/components/tables/roomTable.blade.php
@@ -19,22 +19,6 @@
                 <button @click="getRoom(row.id)" data-toggle="modal" data-target="#editModal">Edit</button>
             </td>
         </tr>
-    <!-- @foreach (json_decode($records) as $record)
-        <tr>
-            <th scope="row">{{ $loop->index + 1 }}</th>
-            <th scope="row">{{ $record->location }}</th>
-            <th scope="row">{{ $record->name }}</th>
-            <th scope="row">{{ $record->number_of_computer }}</th>
-            <th scope="row">
-                <form action="{{ route('admin.room.delete', $record->id) }}" method="post" class="float-right">
-                    @csrf
-                    @method('DELETE')
-                    <button class="btn btn-outline-primary ml-2" onclick="return confirm('Chắc không ?')"><i class="fas fa-trash-alt"></button>
-                </form>
-                <a href="{{ route('admin.room.edit', $record->id) }}"><button class="btn btn-outline-primary float-right"><i class="far fa-edit"></button></a>
-            </th>
-        </tr>
-    @endforeach -->
     </tbody>
 </table>
 
